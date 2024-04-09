3311bd76f841ceeab105c663704da7cb9f4e3333
David Garcia Ruiz <david.garcia@square1.io>
2024-04-09 12:45:03 +0200

    Finish meal detail screen

--- starting-project/screens/MealDetailScreen.js
+++ starting-project/screens/MealDetailScreen.js
@@ -1,24 +1,56 @@
-import { View, Text, Image } from "react-native";
+import { View, Text, Image, StyleSheet, ScrollView } from "react-native";
 
 import { MEALS } from "../data/dummy-data";
 import MealDetails from "../components/MealDetails";
+import Subtitle from "../components/MealDetail/Subtitle";
+import List from "../components/MealDetail/List";
 
 function MealDetailScreen({ route }) {
     const mealId = route.params.mealId;
 
     const selectedMeal = MEALS.find((meal) => meal.id === mealId);
 
-    return <View>
-        <Image source={{ uri: selectedMeal.imageUrl }} />
-        <Text>{selectedMeal.title}</Text>
+    return <ScrollView style={styles.rootContainer}>
+        <Image style={styles.image} source={{ uri: selectedMeal.imageUrl }} />
+        <Text style={styles.title}>{selectedMeal.title}</Text>
         <View>
-            <MealDetails duration={selectedMeal.duration} complexity={selectedMeal.complexity} affordability={selectedMeal.affordability} />
+            <MealDetails textStyle={styles.detailText} duration={selectedMeal.duration} complexity={selectedMeal.complexity} affordability={selectedMeal.affordability} />
         </View>
-        <Text>Ingridients</Text>
-        {selectedMeal.ingredients.map(ingredient => <Text key={ingredient}>{ingredient}</Text>)}
-        <Text>Steps</Text>
-        {selectedMeal.steps.map(step => <Text key={step}>{step}</Text>)}
-    </View>
+        <View style={styles.listOuterContainer}>
+            <View style={styles.listContainer}>
+                <Subtitle>Ingridients</Subtitle>
+                <List data={selectedMeal.ingredients} />
+                <Subtitle>Steps</Subtitle>
+                <List data={selectedMeal.steps} />
+            </View>
+        </View>
+    </ScrollView>
 }
 
 export default MealDetailScreen;
+
+const styles = StyleSheet.create({
+    rootContainer: {
+        marginBottom: 32
+    },
+    image: {
+        width: '100%',
+        height: 350,
+    },
+    title: {
+        fontWeight: 'bold',
+        fontSize: 24,
+        margin: 8,
+        textAlign: 'center',
+        color: 'white'
+    },
+    detailText: {
+        color: 'white'
+    },
+    listOuterContainer: {
+        alignItems: 'center'
+    },
+    listContainer: {
+        width: '80%'
+    }
+});
