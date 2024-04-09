9b35b0c199ecd1586ca9581ca1c2e0c89d41f7b1
David Garcia Ruiz <david.garcia@square1.io>
2024-04-09 14:52:13 +0200

    Add header buttons

--- starting-project/screens/MealDetailScreen.js
+++ starting-project/screens/MealDetailScreen.js
@@ -1,14 +1,28 @@
+import { useLayoutEffect } from 'react';
 import { View, Text, Image, StyleSheet, ScrollView } from "react-native";
 
 import { MEALS } from "../data/dummy-data";
 import MealDetails from "../components/MealDetails";
 import Subtitle from "../components/MealDetail/Subtitle";
 import List from "../components/MealDetail/List";
+import IconButton from '../components/IconButton';
 
-function MealDetailScreen({ route }) {
+function MealDetailScreen({ route, navigation }) {
     const mealId = route.params.mealId;
 
     const selectedMeal = MEALS.find((meal) => meal.id === mealId);
+
+    function headerButtonPressedHandler() {
+        console.log('Pressed');
+    }
+
+    useLayoutEffect(() => {
+        navigation.setOptions({
+            headerRight: () => {
+                return <IconButton icon='star' color='white' onPress={headerButtonPressedHandler} />
+            }
+        });
+    }, [navigation, headerButtonPressedHandler]);
 
     return <ScrollView style={styles.rootContainer}>
         <Image style={styles.image} source={{ uri: selectedMeal.imageUrl }} />
