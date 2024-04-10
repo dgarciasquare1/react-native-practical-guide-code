4b756e9ae0e2364eee263cfcb1607ead2579071e
David Garcia Ruiz <david.garcia@square1.io>
2024-04-10 09:23:46 +0200

    Adding, configuring and using bottom tabs

--- other-navigators-starting-project/App.js
+++ other-navigators-starting-project/App.js
@@ -1,15 +1,17 @@
 import { NavigationContainer } from '@react-navigation/native';
-import { createDrawerNavigator } from '@react-navigation/drawer';
+// import { createDrawerNavigator } from '@react-navigation/drawer';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { Ionicons } from '@expo/vector-icons'
 
 import WelcomeScreen from './screens/WelcomeScreen';
 import UserScreen from './screens/UserScreen';
 
-const Drawer = createDrawerNavigator();
+// const Drawer = createDrawerNavigator();
+const BottomTab = createBottomTabNavigator();
 
 export default function App() {
   return <NavigationContainer>
-    <Drawer.Navigator screenOptions={{
+    {/* <Drawer.Navigator screenOptions={{
       headerStyle: { backgroundColor: '#3c0a6b' },
       headerTintColor: 'white',
       drawerActiveBackgroundColor: '#f0e1ff',
@@ -23,6 +25,18 @@ export default function App() {
       <Drawer.Screen name='User' component={UserScreen} options={{
         drawerIcon: ({ color, size }) => <Ionicons name='person' color={color} size={size} />
       }} />
-    </Drawer.Navigator>
+    </Drawer.Navigator> */}
+    <BottomTab.Navigator screenOptions={{
+      headerStyle: { backgroundColor: '#3c0a6b' },
+      headerTintColor: 'white',
+      tabBarActiveTintColor: '#3c0a6b'
+    }}>
+      <BottomTab.Screen name='Welcome' component={WelcomeScreen} options={{
+        tabBarIcon: ({ color, size }) => <Ionicons name='home' color={color} size={size} />
+      }} />
+      <BottomTab.Screen name='User' component={UserScreen} options={{
+        tabBarIcon: ({ color, size }) => <Ionicons name='person' color={color} size={size} />
+      }} />
+    </BottomTab.Navigator>
   </NavigationContainer>
 }
