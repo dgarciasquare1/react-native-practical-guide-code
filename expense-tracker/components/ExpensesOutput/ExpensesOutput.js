36b7bdcb736aba6d1b4bcb781c17dd9bb7c00d3a
David Garcia Ruiz <david.garcia@square1.io>
2024-04-17 14:20:32 +0200

    Add expenses list items component

--- expense-tracker/components/ExpensesOutput/ExpensesOutput.js
+++ expense-tracker/components/ExpensesOutput/ExpensesOutput.js
@@ -28,6 +28,36 @@ const DUMMY_EXPENSES = [
         description: 'Book',
         amount: 14.59,
         date: new Date('2022-02-18')
+    },
+    {
+        id: 'e5',
+        description: 'A pair of shoes',
+        amount: 59.99,
+        date: new Date('2021-12-19')
+    },
+    {
+        id: 'e6',
+        description: 'A pair of trousers',
+        amount: 89.99,
+        date: new Date('2022-01-05')
+    },
+    {
+        id: 'e7',
+        description: 'Some bananas',
+        amount: 5.99,
+        date: new Date('2021-12-01')
+    },
+    {
+        id: 'e8',
+        description: 'Book',
+        amount: 14.59,
+        date: new Date('2022-02-18')
+    },
+    {
+        id: 'e9',
+        description: 'Book',
+        amount: 14.59,
+        date: new Date('2022-02-18')
     }
 ];
 
@@ -43,7 +73,9 @@ export default ExpensesOutput;
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        padding: 24,
+        paddingHorizontal: 24,
+        paddingTop: 24,
+        paddingBottom: 0,
         backgroundColor: GlobalStyles.colors.primary700
     }
 });
