a5a260ce246eb4618fff2fbfb3ed336730f9e092
David Garcia Ruiz <david.garcia@square1.io>
2024-04-25 11:51:15 +0200

    Authentication error handling

--- 01-starting-code/screens/SignupScreen.js
+++ 01-starting-code/screens/SignupScreen.js
@@ -9,7 +9,14 @@ function SignupScreen() {
 
   async function signupHandler({ email, password }) {
     setIsAuthenticating(true);
-    await createUser(email, password);
+    try {
+      await createUser(email, password);
+    } catch (error) {
+      Alert.alert(
+        "Authentication failed!",
+        "Could not create usr. Please check your input and try again later!"
+      );
+    }
     setIsAuthenticating(false);
   }
 
