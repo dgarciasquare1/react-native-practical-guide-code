913ed00f1fe91af64779f525f3306557e2e9f5c9
David Garcia Ruiz <david.garcia@square1.io>
2024-04-25 13:47:26 +0200

    Storing auth token on the device and logging user in automatically

--- 01-starting-code/App.js
+++ 01-starting-code/App.js
@@ -1,7 +1,9 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { StatusBar } from "expo-status-bar";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import AppLoading from "expo-app-loading";
 
 import LoginScreen from "./screens/LoginScreen";
 import SignupScreen from "./screens/SignupScreen";
@@ -66,11 +68,29 @@ function Navigation() {
 }
 
 export default function App() {
+  function Root() {
+    const [isTryingLogin, setIsTryingLogin] = useState(true);
+    const authCtx = useContext(AuthContext);
+
+    useEffect(() => {
+      async function fetchToken() {
+        const storedToken = await AsyncStorage.getItem("token");
+        if (storedToken) {
+          authCtx.authenticate(storedToken);
+        }
+        setIsTryingLogin(false);
+      }
+      fetchToken();
+    }, []);
+
+    return <Navigation />;
+  }
+
   return (
     <>
       <StatusBar style="light" />
       <AuthContextProvider>
-        <Navigation />
+        <Root />
       </AuthContextProvider>
     </>
   );
